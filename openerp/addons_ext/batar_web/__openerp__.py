1ed332473e5741427de4834b8f2530c89c8cd0c8
xiaomingmeng <xiaomingmeng@batar.cn>
2016-03-29 15:05:37 -0700

    update batar web

---
+++ openerp/addons_ext/batar_web/__openerp__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+{
+    'name': '百泰web',
+    'author': 'Xiao',
+    'description': """
+    修改OD首页，新增样式及JS。LOGO图片等。
+    """,
+    'depends': ['web'],
+    'data': ['views/webclient_templates.xml'],
+
+}
